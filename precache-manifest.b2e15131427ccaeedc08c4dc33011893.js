9c403f3ed9797857a6b143ec79f73444a679021a
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-07-03 10:20:21 +0300

    Updates

---
+++ precache-manifest.b2e15131427ccaeedc08c4dc33011893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c240c3c2eebe5e679e4d1d3b5d77e43b",
+    "url": "/github.io/color-generator/index.html"
+  },
+  {
+    "revision": "56adfb389194566e9e1c",
+    "url": "/github.io/color-generator/static/css/main.11137035.chunk.css"
+  },
+  {
+    "revision": "0b728d42613a7c9ec6d3",
+    "url": "/github.io/color-generator/static/js/2.cdd6e70d.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/github.io/color-generator/static/js/2.cdd6e70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56adfb389194566e9e1c",
+    "url": "/github.io/color-generator/static/js/main.7cddb31b.chunk.js"
+  },
+  {
+    "revision": "6168e0e9a05812eef5d6",
+    "url": "/github.io/color-generator/static/js/runtime-main.7fe19457.js"
+  }
+]);
